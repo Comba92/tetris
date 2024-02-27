bf192fc14d5d154fe006e03f8a52bb6171d28c54
Comba <comba@cock.li>
2024-02-27 19:04:51 +0100

    Added input handling

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 
 // 4:3 = 640x480, 800x600, 1024x768 -  16:9 = 1280x720, 1920x1080
 
+static const int CELL_SIZE = 20;
+
 typedef struct {
   int width;
   int height;
@@ -14,9 +16,20 @@ typedef struct {
 
 typedef enum {
   tT, tS, tZ, tI, tJ, tL, tO
+} TetrominoType;
+
+typedef enum {
+  r0, r90, r180, r270
+} Rotation;
+
+typedef struct {
+  TetrominoType type;
+  Rotation rotation;
+  int x;
+  int y;
 } Tetromino;
 
-static const unsigned int tetrominoes[7][4] = {
+static const unsigned int TETROMINOES[7][4] = {
   { 0x4640, 0x0E40, 0x4C40, 0x4E00 }, // 'T'
   { 0x8C40, 0x6C00, 0x8C40, 0x6C00 }, // 'S'
   { 0x4C80, 0xC600, 0x4C80, 0xC600 }, // 'Z'
@@ -26,21 +39,99 @@ static const unsigned int tetrominoes[7][4] = {
   { 0xCC00, 0xCC00, 0xCC00, 0xCC00 }  // 'O'
 };
 
+typedef void (*KeyCallback)(Tetromino* t);
+typedef struct {
+  int key;
+  KeyCallback callback;
+} KeyCommand;
+
+void moveLeft(Tetromino* t)   { t->x -= 1; }
+void moveRight(Tetromino* t)  { t->x += 1; }
+void moveUp(Tetromino* t)     { t->y -= 1; }
+void moveDown(Tetromino* t)   { t->y += 1; }
+
+typedef enum {
+  LEFT, RIGHT, UP, DOWN
+} Keys;
+static const KeyCommand KEY_COMMANDS[4] = {
+  (KeyCommand) { .key = KEY_LEFT,   .callback = &moveLeft },
+  (KeyCommand) { .key = KEY_RIGHT,  .callback = &moveRight },
+  (KeyCommand) { .key = KEY_UP,     .callback = &moveUp },
+  (KeyCommand) { .key = KEY_DOWN,   .callback = &moveDown },
+};
+
+void handleInputAndUpdateTetromino(Tetromino* t) {
+  for(int i=0; i<4; i++) {
+    KeyCommand k = KEY_COMMANDS[i];
+    if (IsKeyDown(k.key) && k.callback) k.callback(t);
+  }
+}
+
+Grid initGrid(int width, int height) {
+  Grid grid;
+  grid.width = width;
+  grid.height = height;
+  grid.blocks = malloc(width * height * sizeof(bool));
+
+  return grid;
+}
+
+Grid updateGrid(Grid grid, Tetromino t) {
+
+}
+
+void drawLines(Grid grid) {
+  for (int y = 0; y <= grid.height; y++) {
+    DrawLine(0, y * CELL_SIZE, grid.width * CELL_SIZE, y * CELL_SIZE, GRAY);
+  }
+
+    for (int x = 0; x <= grid.width; x++) {
+    DrawLine(x * CELL_SIZE, 0, x * CELL_SIZE, grid.height * CELL_SIZE, GRAY);
+  }
+}
+
+void drawSprite(Tetromino t) {
+  // Sprite boundaries
+  DrawRectangleLines(t.x * CELL_SIZE, t.y * CELL_SIZE, 4 * CELL_SIZE, 4 * CELL_SIZE, RED);
+
+  for (int off_y = 0; off_y < 4; off_y++) {
+    for (int off_x = 0; off_x < 4; off_x++) {
+      // TODO: extract this condition to function
+      if (TETROMINOES[t.type][t.rotation] & (0x8000 >> (off_y * 4 + off_x))) {
+        DrawRectangleLines(
+          (t.x + off_x) * CELL_SIZE, 
+          (t.y + off_y) * CELL_SIZE, 
+          CELL_SIZE, CELL_SIZE, WHITE
+        );
+      }
+    }
+  }
+}
+
+Tetromino newTetromino(TetrominoType type, int x, int y) {
+  Tetromino t;
+  t.type = type;
+  t.rotation = r0;
+  t.x = x;
+  t.y = y;
+
+  return t;
+}
+
 int main() {
   InitWindow(800, 600, "Tetris");
   SetTargetFPS(30);
 
+  Grid grid = initGrid(30, 60);
+  Tetromino current = newTetromino(tT, 0, 0);
+
   while(!WindowShouldClose()) {
     BeginDrawing();
       ClearBackground(BLACK);
+      drawLines(grid);
+      drawSprite(current);
 
-      for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++) {
-          if (tetrominoes[tT][0] & (0x8000 >> (y * 4 + x))) {
-            DrawRectangleLines(x * 20, y * 20, 20, 20, WHITE);
-          }
-        }
-      }
+      handleInputAndUpdateTetromino(&current);
     EndDrawing();
   }
 
